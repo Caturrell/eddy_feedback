e08a1e0770acb3913ecb12bdb3b7d6fa368ce9ae
caturrell <charlie.turrell@hotmail.co.uk>
2024-10-28 19:51:48 +0000

    HadGEM3 resolution difference shown. Also some file management.

--- chapter1/resolution/data/calculate_pamip_divFy.py
+++ chapter1/resolution/data/calculate_pamip_divFy.py
@@ -2,25 +2,25 @@ import xarray as xr
 
 import pdb
 import warnings
-
-
 import logging
 import os
-import sys
-sys.path.append('/home/links/ct715/eddy_feedback/')
+
 import functions.eddy_feedback as ef
+import functions.data_wrangling as data
 
 # define function
 def process_monthly_pamip_data(ua, epfy):
     """ Process ua and epfy variables, put into new dataset,
         calculate ubar and divFy """
-    model = xr.Dataset({'ubar': ua.ua.mean('lon'), 'epfy': epfy.epfy})
+    model = xr.Dataset({'ubar': ua.u.mean('lon'), 'epfy': epfy.epfy})
     model = ef.calculate_divFphi(model)
     return model
 
 
 # remove_models = ['FGOALS-f3-L', 'IPSL-CM6A-LR', 'MIROC6_mass', 'HadGEM3-GC31-LL', \
 #                  'OpenIFS-1279', 'OpenIFS-511', 'OpenIFS-159']
+
+
 
 if __name__ == '__main__':
     
@@ -34,16 +34,20 @@ if __name__ == '__main__':
         format='%(asctime)s - %(levelname)s - %(message)s'
 )
     
-    # Extract model names from the directory
+    # set directory path
     path = '/home/links/ct715/data_storage/PAMIP/monthly/1.1_pdSST-pdSIC/'
-    model_names = os.listdir(path + '/ua')
-    model_names.sort()
+    # # Extract model names from the directory
+    # model_names = os.listdir(path + '/ua')
+    # model_names.sort()
+    
+    missing_data_models = ['CanESM5', 'CESM2', 'HadGEM3-GC31-MM', 'MIROC6', 'NorESM2-LM']
+    model_names = missing_data_models
 
     # Loop over each model
     for model in model_names:
         logging.info(f'Processing {model}...')
         
-        # define paths for data 
+        # define paths for data of each variable
         ua_data_path = os.path.join(path, 'ua', model, '*.nc')
         epfy_data_path = os.path.join(path, 'epfy', model, '*.nc')
         
@@ -57,6 +61,7 @@ if __name__ == '__main__':
                 concat_dim='ens_ax', 
                 combine='nested'
             )
+            ua = data.data_checker1000(ua)
         except Exception as e:
             logging.error(f'Error opening dataset for {model}, variable ua: {e}')
             continue  # Skip to the next variable/model if there's an error
@@ -70,9 +75,21 @@ if __name__ == '__main__':
                 concat_dim='ens_ax', 
                 combine='nested'
             )
+            epfy = data.data_checker1000(epfy)
         except Exception as e:
             logging.error(f'Error opening dataset for {model}, variable epfy: {e}')
             continue  # Skip to the next variable/model if there's an error
+        
+        if not ua.level.equals(epfy.level):
+            if len(ua.level) > len(epfy.level):
+                ua = ua.interp(level=epfy.level)
+                epfy = epfy
+            else:
+                ua = ua
+                epfy = epfy.interp(level=ua.level)
+        else:
+            ua, epfy = ua, epfy  # Use original DataArrays if levels match
+
         
         # now attempt to process data
         try:
